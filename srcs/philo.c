889990c003d4929a7bdf3545018756147346264e
Elvira Martínez López <elvmarti@MacBook-Pro-de-Carlos.local>
2021-11-25 18:41:06 +0100

    list working

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: elvmarti <elvmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 17:27:28 by elvmarti          #+#    #+#             */
-/*   Updated: 2021/11/23 18:52:17 by elvmarti         ###   ########.fr       */
+/*   Updated: 2021/11/25 18:33:49 by elvmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	*make_thread(void *param)
 {
-	t_philo 	*phil;
-	
-	phil = (t_philo *)param;
-	printf("Num filo: %d\n", phil->num_philo);
+	t_philolist	*philo;
+
+	philo = (t_philolist *)param;
+	printf("Num filo: %d\n", philo->philo.num);
 	return (NULL);
 }
 
-static void make_args(t_philo *phil, int argc, char **argv)
+static void	make_args(t_philo *phil, int argc, char **argv)
 {
 	phil->num_philo = ft_atoi(argv[1]);
 	phil->time_die = ft_atoi(argv[2]);
@@ -33,21 +33,47 @@ static void make_args(t_philo *phil, int argc, char **argv)
 		print_error();
 }
 
-int main(int argc, char **argv)
+static int	check_args(int argc, char **argv)
 {
-	pthread_t	philo;
-	t_philo		phil;
-	int			i;
-	
-	ft_bzero(&phil, sizeof(phil));
+	int	i;
+	int	y;
+
 	if (argc < 5 || argc > 6)
-		print_error();
+		return (print_error());
+	i = 1;
+	while (argv[i])
+	{
+		y = 0;
+		while (argv[i][y])
+		{
+			if (!ft_isdigit(argv[i][y]))
+				return (print_error());
+			y++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	pthread_t	thread;
+	t_philo		phil;
+	t_philolist	*list;
+	int			i;
+
+	ft_bzero(&phil, sizeof(phil));
+	if (check_args(argc, argv))
+		return (1);
 	make_args(&phil, argc, argv);
+	list = create_list(&phil);
 	i = 0;
 	while (i < phil.num_philo)
 	{
-		pthread_create(&philo, NULL, make_thread, (void *)&phil);
-		pthread_join(philo, NULL);
+		pthread_create(&thread, NULL, make_thread, (void *)list);
+		pthread_join(thread, NULL);
+		list = list->next;
 		i++;
 	}
+	return (0);
 }
--- srcs/utils.c
+++ srcs/utils.c
@@ -6,14 +6,61 @@
 /*   By: elvmarti <elvmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 18:41:19 by elvmarti          #+#    #+#             */
-/*   Updated: 2021/11/23 18:47:09 by elvmarti         ###   ########.fr       */
+/*   Updated: 2021/11/25 18:40:08 by elvmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-int	print_error()
+int	print_error(void)
 {
 	printf("Error\n");
 	return (1);
+}
+
+int	ft_isdigit(int a)
+{
+	if (a > 47 && a < 58)
+		return (1);
+	return (0);
+}
+
+void	ft_bzero(void *pt, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)pt)[i] = '\0';
+		i++;
+	}
+}
+
+int	ft_atoi(char *str)
+{
+	long long int	signo;
+	long long int	sol;
+
+	signo = 1;
+	sol = 0;
+	while (*str == ' ' || *str == '\n' || *str == '\f'
+		|| *str == '\r' || *str == '\t' || *str == '\v')
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			signo = -signo;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		if (sol * signo > 2147483648)
+			return (-1);
+		else if (sol * signo < -2147483648)
+			return (0);
+		sol = sol * 10 + (*str - 48);
+		str++;
+	}
+	return (signo * sol);
 }
